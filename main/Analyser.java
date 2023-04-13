b721a68ffff5074978803b98c235bdfa598ef851
GnohChengYi <chengyignoh@gmail.com>
2023-04-13 10:40:59 +0800

    remove debug printing

--- main/Analyser.java
+++ main/Analyser.java
@@ -9,20 +9,13 @@ class Analyser {
     }
 
     List<String> getMatchingQueries(String queryStrHas, String missingHas, String additionalHas) {
-        System.out.printf("analysing: queryStrRegex=%s missingRegex=%s additionalRegex=%s\n\n", queryStrHas,
-                missingHas, additionalHas);
         List<Query> failedQueries = testResults.getFailedQueries();
         List<String> ids = new ArrayList<>();
         for (Query query : failedQueries) {
-            System.out.println("id: " + query.getId());
-            System.out.println("querystr: " + query.getQueryStr());
-            System.out.println("missing: " + query.getFailed().getMissing());
-            System.out.println("additional: " + query.getFailed().getAdditional());
             if (query.getQueryStr().contains(queryStrHas) && query.getFailed().getMissing().contains(missingHas)
                     && query.getFailed().getAdditional().contains(additionalHas)) {
                 ids.add(query.getId());
             }
-            System.out.println();
         }
         return ids;
     }
